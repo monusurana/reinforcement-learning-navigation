1b44fc5ae6d0d177810d5203c3c4e2a42091f82c
Monu Surana <msurana@qti.qualcomm.com>
2020-03-01 20:24:24 -0800

    Added Dueling DQN agent support

---
+++ networks/dueling_qnetwork.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DuelingQNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(DuelingQNetwork, self).__init__()
+        self.feature = nn.Sequential(
+            nn.Linear(state_size, 64),
+            nn.ReLU()
+        )
+        
+        self.advantage = nn.Sequential(
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, action_size)
+        )
+        
+        self.value = nn.Sequential(
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, 1)
+        )
+        
+    def forward(self, x):
+        x = self.feature(x)
+        advantage = self.advantage(x)
+        value     = self.value(x)
+        
+        return value + advantage  - advantage.mean()
